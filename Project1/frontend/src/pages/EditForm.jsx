ee25386e5f0d63fc9c5b1619644a7033778f55f0
Sagar kumar <srk001929@gmail.com>
2026-07-22 12:31:48 +0530

    added edit form

--- Project1/frontend/src/pages/EditForm.jsx
+++ Project1/frontend/src/pages/EditForm.jsx
@@ -1,8 +1,53 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function EditForm() {
-    return (
-        <>
-        </>
-    )
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    age: 0,
+    password: "",
+  });
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+  return (
+    <>
+      <form>
+        <h1>Edit User</h1>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          name="name"
+          onChange={handleChange}
+        ></input>
+        <br />
+        <input
+          type="number"
+          placeholder="Enter your age"
+          name="age"
+          onChange={handleChange}
+        ></input>
+        <br />
+        <input
+          type="email"
+          placeholder="Enter your email"
+          name="email"
+          onChange={handleChange}
+        ></input>
+        <br />
+
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default EditForm;
