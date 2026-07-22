33ed5a628971ec0883a60472925c4a18f670fbac
Sagar kumar <srk001929@gmail.com>
2026-07-22 13:03:44 +0530

    added edit form update

--- Project1/frontend/src/pages/EditForm.jsx
+++ Project1/frontend/src/pages/EditForm.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState , useEffect} from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getAllUsers } from "../services/userService";
 
 function EditForm() {
   const navigate = useNavigate();
+  const { id } = useParams();
 
   const [formData, setFormData] = useState({
     name: "",
@@ -10,21 +12,58 @@ function EditForm() {
     age: 0,
     password: "",
   });
+  const [user, setUser] = useState("");
+
+  async function fetchUser() {
+    try {
+      const res = await getAllUsers();
+      const user = res.data.users.find((u) => u._id === id);
+      setUser(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   function handleChange(e) {
+    e.preventDefault();
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
   }
+
+  async function update(e) {
+    e.preventDefault();
+
+    try {
+      const res = await editUser(id, formData);
+      alert(res.data.message);
+      navigate("/");
+
+      setFormData({
+        name: "",
+        email: "",
+        age: "",
+        password: "",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
-      <form>
+      <form onSubmit={update}>
         <h1>Edit User</h1>
         <input
           type="text"
           placeholder="Enter your name"
           name="name"
+          value={user.name}
           onChange={handleChange}
         ></input>
         <br />
@@ -32,6 +71,7 @@ function EditForm() {
           type="number"
           placeholder="Enter your age"
           name="age"
+          value={user.age}
           onChange={handleChange}
         ></input>
         <br />
@@ -39,6 +79,7 @@ function EditForm() {
           type="email"
           placeholder="Enter your email"
           name="email"
+          value={user.email}
           onChange={handleChange}
         ></input>
         <br />
